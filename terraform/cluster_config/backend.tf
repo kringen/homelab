f81da4dcde2b6c5757b08aec9db840f21c158fb5
Erik Kringen <ekringen@gmail.com>
2023-09-22 18:41:28 -0400

    Added kubernetes cluster deploy

---
+++ terraform/cluster_config/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "kringlab"#var.tfc_organization
+
+    workspaces {
+      name = "kringlab-cluster"#var.tfc_workspace
+    }
+  }
+}
