1488f75b92b539f56d8ee08576918a5146f11194
Erik Kringen <erik@kringlab01.lab.kringen.local>
2024-11-16 12:56:26 -0500

    adding test

---
+++ packer/test/backend.tf
@@ -0,0 +1,11 @@
+/*terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "kringlab"#var.tfc_organization
+
+    workspaces {
+      name = "kringlab-ubuntu-test"#var.tfc_workspace
+    }
+  }
+}
+*/
---
+++ packer/test/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+    ignition = {
+      source = "community-terraform-providers/ignition"
+      version = "2.3.5"
+    }
+  }
+}
